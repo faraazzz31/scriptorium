ef3a961260a33e7c5df3562d5bf4425781840c2e
Christoffer Tan <tanchristoffer@gmail.com>
2024-10-23 19:00:01 -0400

    Edited the prisma schema for CodeTemplate and Added new endpoints to Fetch tags and save code-template

---
+++ PP1/app/api/code-template/save/route.js
@@ -0,0 +1,61 @@
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+import { withAuth } from '@/app/middleware/auth';
+
+const prisma = new PrismaClient();
+
+async function handler (req) {
+    const user = req.user;
+    console.log(`user: ${JSON.stringify(user)}`);
+
+    if (!user) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    try {
+        const { title, explanation, code, language, tag_ids } = await req.json();
+        console.log(`title: ${title}, code: ${code}, explanation: ${explanation}, tags: ${tag_ids}, language: ${language}`);
+
+        if (!title || !code || !language) {
+            return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
+        }
+
+        const data = {
+            title: title,
+            code: code,
+            language: language,
+            explanation: explanation? explanation : '',
+            author: {
+                connect: {
+                    id: user.id,
+                },
+            },
+        }
+
+        if (tag_ids && tag_ids.length > 0) {
+            data.tags = {
+                connect: tag_ids.map(tag_id => ({ id: tag_id })),
+            };
+        }
+
+        const codeTemplate = await prisma.codeTemplate.create({
+            data: data,
+        });
+
+        return NextResponse.json(
+            {
+                id: codeTemplate.id,
+                title: codeTemplate.title,
+                explanation: codeTemplate.explanation,
+                code: codeTemplate.code,
+                author: codeTemplate.author,
+                tags: codeTemplate.tags,
+            }
+        );
+    } catch (error) {
+        console.error(`Error in /app/api/code_template/save: ${error}`);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
+
+export const POST = withAuth(handler);
---
+++ PP1/app/api/tag/fetch/route.js
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+import { withAuth } from '@/app/middleware/auth';
+
+const prisma = new PrismaClient();
+
+async function handler (req) {
+    const user = req.user;
+
+    if (!user) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    try {
+        const tags = await prisma.tag.findMany({
+            select: {
+                id: true,
+                name: true,
+            },
+        });
+
+        return NextResponse.json(
+            tags,
+            { status: 200 });
+    } catch (error) {
+        console.error(`Error in /app/api/tag/fetch: ${error}`);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
+
+export const GET = withAuth(handler);
